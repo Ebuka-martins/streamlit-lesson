73027dc6dc2631a85721ad0180f11cc207150118
Ebuka-martins <martin4best@gmail.com>
2025-02-20 13:27:34 +0000

    docs: create streamlite app

--- 2_app_with_multiple_pages.py
+++ 2_app_with_multiple_pages.py
@@ -1,1 +1,15 @@
+import streamlit as st
+from app_pages.multi_page import MultiPage
 
+from app_pages.page1 import page1_body
+from app_pages.page2 import page2_body
+
+# Create an instance
+app = MultiPage(app_name="This is my first multi-page Streamlit App")
+
+# Add pages using the correct method name
+app.app_page("Page 1", page1_body)
+app.app_page("Page 2", page2_body)
+
+# Run the app
+app.run()
